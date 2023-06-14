1469491182f0088ee5d2157d5763b61314016c53
Hamzamt99 <tamarihamza4@gmail.com>
2023-06-14 09:01:21 +0000

    done all testing and 500 error handler

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,11 +1,13 @@
 'use strict'
 
 // error parameter not working
-module.exports = function Error500(req,res){
+module.exports = function Error500(error,req,res,next){
     res.status(500).json({
         Errorcode: 500,
-        message: 'Code Error',
+        message: `Code Error message:${error.message}` ,
         path : req.originalUrl 
     })
 
 }
+
+// 
